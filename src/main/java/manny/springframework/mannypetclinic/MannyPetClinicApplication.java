5e8e6e7a5420229c93d7301c87ca4c6f0bf44f94
MannyCalavera92 <eduardo.rodriguez.llorente@outlook.com>
2019-02-04 09:51:47 +0100

    Initialized project with Spring Initializr

---
+++ src/main/java/manny/springframework/mannypetclinic/MannyPetClinicApplication.java
@@ -0,0 +1,14 @@
+package manny.springframework.mannypetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannyPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MannyPetClinicApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/manny/springframework/mannypetclinic/MannyPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package manny.springframework.mannypetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MannyPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
